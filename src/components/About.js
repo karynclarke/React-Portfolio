015f04b530b623c76c710cae12f4c4f974678b8a
karynclarke <karynclarke@comcast.net>
2020-10-16 17:45:24 -0700

    class names update

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ function About() {
 	
 			
 			<p>
-			<img class="images " img src="assets/karynpic.JPG " alt="Karyn " />
+			<img className="images " img src="assets/karynpic.JPG " alt="Karyn " />
 			<div className="words">
 			I am an October, 2020 Coding BootCamp graduate, learning Full Stack Development at an accelerated pace.   My Portfolio shows a variety of homework assignments done in class and a group assigment.   When I am not coding, I use my creative and technical abilities daily in my career as TV Newscast Director.   I am looking for an opportunity to utilize my experience working with teams, adapting to changing environments, and learning new technologies.
 			</div>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,20 +17,17 @@ function Footer() {
 				</div>
 
 				<div className="row" id="icons ">
-					<a href="https://github.com/karynclarke/React-Portfolio/" target="_blank" rel="noopener">
-						<div className="icons">
-							<i className="fa fa-github fa-4x" />
-						</div>
+					<Link to="https://github.com/karynclarke/React-Portfolio/" target="_blank" rel="noopener"></Link>
+					<div className="icons">
+						<i className="fa fa-github fa-4x" />
+					</div>
 
-						<a href="https://www.linkedin.com/in/karyn-clarke-portlandor/" target="_blank" rel="noopener">
-							<div className="icons">
-								<i className="fa fa-linkedin fa-4x" />{' '}
-							</a>
-						</div>
+					<Link to="https://www.linkedin.com/in/karyn-clarke-portlandor/" target="_blank" rel="noopener"></Link>
+					<div className="icons">
+						<i className="fa fa-linkedin fa-4x" />{' '}
 					</div>
 				</div>
-
-				<div class="row " id="resume-row ">
+				<div className="resume-row ">
 					<a href="BootCampResume.pdf">
 						<h3> See My Resume Here </h3>
 					</a>
