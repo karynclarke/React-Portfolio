8c12315a64b5cab9d283e3530f9bdc9df00854e3
karynclarke <karynclarke@comcast.net>
2020-10-24 16:35:20 -0700

    image correction

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,8 @@ function About() {
 	return (
 		<div className="aboutContainer">
 			<p>
-				<img className="images bgbx " img src="assets/karynpic.JPG " alt="Karyn " />
+				{/* <img className="images bgbx " img src="assets/karynpic.JPG " alt="Karyn " /> */}
+				<img className="images bgbx" src={public.env.PUBLIC_URL + '/assets/karynpic.JPG'} alt="Karyn" />
 				<div className="words-about">I am an October, 2020 Coding BootCamp graduate, learning Full Stack Development at an accelerated pace. My Portfolio shows a variety of homework assignments done in class and a group assigment. When I am not coding, I use my creative and technical abilities daily in my career as TV Newscast Director. I am looking for an opportunity to utilize my experience working with teams, adapting to changing environments, and learning new technologies.</div>
 			</p>
 		</div>
