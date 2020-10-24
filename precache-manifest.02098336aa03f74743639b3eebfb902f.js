00437f0d2e80049c64396f63fc2d33090327364c
karynclarke <karynclarke@comcast.net>
2020-10-24 14:14:11 -0700

    Updates

---
+++ precache-manifest.02098336aa03f74743639b3eebfb902f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d9100c6a829795b40ebd5116471a98",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "a9a836903d6a4ba9be0a",
+    "url": "/react-portfolio/static/css/main.5267cee0.chunk.css"
+  },
+  {
+    "revision": "191bc52c87b25f7f2d83",
+    "url": "/react-portfolio/static/js/2.30f7fb1a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-portfolio/static/js/2.30f7fb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a836903d6a4ba9be0a",
+    "url": "/react-portfolio/static/js/main.b925d44e.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
