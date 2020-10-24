4b9ddaa737a0e2505b3f00125e9e3b3994e41f52
karynclarke <karynclarke@comcast.net>
2020-10-24 16:18:33 -0700

    Updates

---
+++ precache-manifest.399a154853f184839e289feb5eb45db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b69f2475e029200d802f5d882fe5e8",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "a9a836903d6a4ba9be0a",
+    "url": "/React-Portfolio/static/css/main.5267cee0.chunk.css"
+  },
+  {
+    "revision": "191bc52c87b25f7f2d83",
+    "url": "/React-Portfolio/static/js/2.30f7fb1a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/React-Portfolio/static/js/2.30f7fb1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9a836903d6a4ba9be0a",
+    "url": "/React-Portfolio/static/js/main.b925d44e.chunk.js"
+  },
+  {
+    "revision": "bcebe2d38e8b197ca4b9",
+    "url": "/React-Portfolio/static/js/runtime-main.266d4783.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.02098336aa03f74743639b3eebfb902f.js"
+  "/React-Portfolio/precache-manifest.399a154853f184839e289feb5eb45db1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
