514d49232cfabd6e060c87638baad6773a17ed3f
karynclarke <karynclarke@comcast.net>
2020-10-24 19:52:10 -0700

    752

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,13 @@
 import React, {Component} from 'react';
 import './About.css';
-import selfie from '../assets/karynpic.JPG';
-
-
+import Selfie from '../assets/karynpic.JPG';
 
 function About() {
 	return (
 		<div className="aboutContainer">
 			<p>
 				
-				<img className="images bgbx " img src={selfie} alt="Karyn " />
+				<img className="images bgbx " img src={Selfie} alt="Karyn " />
 				
 				<div className="words-about">I am an October, 2020 Coding BootCamp graduate, learning Full Stack Development at an accelerated pace. My Portfolio shows a variety of homework assignments done in class and a group assigment. When I am not coding, I use my creative and technical abilities daily in my career as TV Newscast Director. I am looking for an opportunity to utilize my experience working with teams, adapting to changing environments, and learning new technologies.</div>
 			</p>
